09a024f9e80f0e48b5b517101684c5ab359a099d
Miguel Silva <miguasjpsilva@gmail.com>
2019-06-28 02:12:45 +0100

    Added Project

---
+++ Mail Info/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mail_Info.scripts;
+using System.IO;
+
+namespace Mail_Info
+{
+    public partial class MainForm : Form
+    {
+        OpenFileDialog openFile;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            openFile = new OpenFileDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(openFile.FileName))
+            {
+                MessageBox.Show("Path not valid: " + openFile.FileName);
+                return;
+            }
+
+            richTextBox1.Text = string.Empty;
+
+            List<int> fileIndexes = Parser.ParseFile(textBox1.Text);
+
+            for (int i = 0; i < fileIndexes.Count; i++)
+            {
+                richTextBox1.Text += fileIndexes[i];
+                richTextBox1.Text += Environment.NewLine;
+            }
+        }
+
+        private void textBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFile.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image  
+            // assigned to Button2.  
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "Save Text File";
+            saveFileDialog1.Filter = "Text File|*.txt";  
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+            }
+        }
+    }
+}
---
+++ Mail Info/scripts/Parser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Mail_Info.scripts
+{
+    /*
+    struct MailInfo
+    {
+        public string Mail  { get; set; }
+        public string Password  { get; set; }
+        public string Date { get; set; }
+        public int Line { get; set; }
+    }
+    */
+
+    class Parser
+    {
+        static public List<int> ParseFile(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+
+            List<int> mailIndexes = new List<int>();
+
+            for (int i = 0; i < file.Length; i++)
+			{
+
+			    string[] firstSplit = file[i].Split('|');
+                string[] daSplit = file[i].Split('|');
+
+                string[] accountSplit = firstSplit[0].Split(':');
+                string[] dateSplit = firstSplit[1].Split(':');
+
+                string date = dateSplit[1].Substring(1);
+                date = date.Split(' ')[0];
+
+                DateTime dateLimit = new DateTime(2019, 7, 1);
+                DateTime dateTime = DateTime.Parse(date);
+
+                if (dateTime >= dateLimit)
+                {
+                    mailIndexes.Add(i + 1);   
+                }
+			}
+
+            return mailIndexes;
+        }
+    }
+}
